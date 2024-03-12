413daccb10ae5473f0e480a39df67e9b6af6d8f3
Frank <frankhuang66655@gmail.com>
2024-03-12 09:10:55 +0800

    Add project files.

---
+++ SmartMan2FA/Program.cs
@@ -0,0 +1,108 @@
+﻿using Google.Authenticator;
+
+namespace SmartMan2FA
+{
+    internal class Program
+    {
+        // -g -a FrankHuang -k mySecretKey to generate a new setup code
+        // -v -k mySecretKey -p 123456 to validate a code
+        // return 0 if the code is valid or the qrcode is generated successfully, otherwise return -1
+        static int Main(string[] args)
+        {
+            // success code = 0, error code = -1
+            int successCode = 0;
+            int errorCode = -1;
+
+            // Create a logger
+            var logger = new SmartManLogger("logs");
+            logger.LogText("Starting SmartMan2FA.");
+            try
+            {
+                // Check if the arguments are valid
+                bool generateSetupCode = Array.IndexOf(args, "-g") != -1;
+                bool validateCode = Array.IndexOf(args, "-v") != -1;
+                if (!generateSetupCode && !validateCode)
+                {
+                    logger.LogText("Invalid arguments. Please use the format '-g -a FrankHuang -k mySecretKey' to generate a new setup code or '-v -k mySecretKey -p 123456' to validate a pin code.");
+                    return errorCode;
+                }
+                if (generateSetupCode)
+                {
+                    // accept the arguments based on the format '-a FrankHuang -k mySecretKey'
+                    // check if the arguemnt is valid
+                    if (args.Length < 4 || Array.IndexOf(args, "-a") == -1 || Array.IndexOf(args, "-k") == -1)
+                    {
+                        logger.LogText("Invalid arguments. Please use the format '-a FrankHuang -k mySecretKey'.");
+                        return errorCode;
+                    }
+                    // get the account and secret key
+                    string account = args[Array.IndexOf(args, "-a") + 1];
+                    string secretKey = args[Array.IndexOf(args, "-k") + 1];
+                    // create a new instance of the TwoFactorAuthenticator
+                    var tfa = new TwoFactorAuthenticator();
+                    // generate a new setup code
+                    SetupCode setupInfo = tfa.GenerateSetupCode("SmartMan2FA", account, secretKey, false);
+                    logger.LogText("Setup code and QRCode have been generated successfully.");
+                    // write the manual entry setup code to a text file
+                    string manualEntrySetupCode = setupInfo.ManualEntryKey;
+                    // write the QR code setup code to a text file
+                    string setupCodeFileName = "setup_code.txt";
+                    using (StreamWriter file = new StreamWriter(setupCodeFileName))
+                    {
+                        file.WriteLine(manualEntrySetupCode);
+                    }
+                    logger.LogText($"Setup code has been output to {setupCodeFileName}.");
+                    string qrCodeBase64String = setupInfo.QrCodeSetupImageUrl;
+                    // extract the base64 QRCode from the setup code
+                    qrCodeBase64String = qrCodeBase64String.Split(new string[] { "base64," }, StringSplitOptions.None)[1];
+                    // convert the base64 QRCode to JPG
+                    byte[] qrCodeBytes = Convert.FromBase64String(qrCodeBase64String);
+                    string qrCodeFileName = "qrcode.jpg";
+                    using (FileStream file = new FileStream(qrCodeFileName, FileMode.Create))
+                    {
+                        file.Write(qrCodeBytes, 0, qrCodeBytes.Length);
+                    }
+                    logger.LogText($"QRCode has been output to {qrCodeFileName}.");
+                    return successCode;
+                }
+                else if (validateCode)
+                {
+                    // check if the arguemnt is valid
+                    if (args.Length < 4 || Array.IndexOf(args, "-k") == -1 || Array.IndexOf(args, "-p") == -1)
+                    {
+                        logger.LogText("Invalid arguments. Please use the format '-k mySecretKey -p 123456'.");
+                        return errorCode;
+                    }
+                    // get the secret key and pin code
+                    string secretKey = args[Array.IndexOf(args, "-k") + 1];
+                    string pinCode = args[Array.IndexOf(args, "-p") + 1];
+                    // create a new instance of the TwoFactorAuthenticator
+                    var tfa = new TwoFactorAuthenticator();
+                    // validate the pin code
+                    bool pinCodeIsValid = tfa.ValidateTwoFactorPIN(secretKey, pinCode);
+                    if (pinCodeIsValid)
+                    {
+                        logger.LogText("The pin code is valid.");
+                        return successCode;
+                    }
+                    else
+                    {
+                        logger.LogText("The pin code is invalid.");
+                        return errorCode;
+                    }
+                }
+                logger.LogText("Unexpected error.");
+                return errorCode;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("An unhandled exception occurred.", ex);
+                return errorCode;
+            }
+            finally
+            {
+                logger.LogText("Exiting SmartMan2FA.");
+            }
+        }
+    }
+}
---
+++ SmartMan2FA/SmartManLogger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMan2FA
+{
+    internal class SmartManLogger
+    {
+        private readonly string logDirectory;
+        public SmartManLogger(string logDirectory)
+        {
+            string logFileName = $"smartman2fa_{DateTime.Now:yyyyMMdd}.txt";
+            if (string.IsNullOrEmpty(logDirectory))
+            {
+                this.logDirectory = logFileName;
+            }
+            else
+            {
+                // Check if the logDirectory is a fully qualified path
+                if (!Path.IsPathFullyQualified(logDirectory))
+                {
+                    // Combine with the base directory
+                    logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, logDirectory);
+                }
+
+                // Ensure the directory exists
+                if (!Directory.Exists(logDirectory))
+                {
+                    Directory.CreateDirectory(logDirectory);
+                }
+
+                this.logDirectory = Path.Combine(logDirectory, logFileName);
+            }
+        }
+        public void LogText(string message)
+        {
+            string formattedMessage = $"{DateTime.Now:yyyy/MM/dd HH:mm:ss} --- {message}";
+            Console.WriteLine(formattedMessage);
+            using var logFile = new StreamWriter(logDirectory, true);
+            logFile.WriteLine(formattedMessage);
+        }
+        public void LogError(string message, Exception? ex = null)
+        {
+            string formattedMessage = $"{DateTime.Now:yyyy/MM/dd HH:mm:ss} --- [Error] {message}";
+            if (ex != null)
+            {
+                formattedMessage += $" {ex.Message}, [StackTrace] {ex.StackTrace}";
+            }
+            Console.WriteLine(formattedMessage);
+            using var logFile = new StreamWriter(logDirectory, true);
+            logFile.WriteLine(formattedMessage);
+        }
+        // Delete old log files
+        public void DeleteOldLogs(int days)
+        {
+            // Get the directory path
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string? logFolder = Path.GetDirectoryName(Path.Combine(baseDirectory, this.logDirectory));
+
+            // If the directory path is not fully qualified, and the directory does not exist, return
+            if (logFolder == null || !Path.IsPathFullyQualified(logFolder) && !Directory.Exists(logFolder))
+            {
+                return;
+            }
+            var files = Directory.GetFiles(logFolder).Where(f => f.StartsWith("encryptor_log_"));
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.CreationTime < DateTime.Now.AddDays(-days))
+                {
+                    fileInfo.Delete();
+                }
+            }
+        }
+    }
+}
